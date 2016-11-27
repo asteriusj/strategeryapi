2620faa51134308b92a7fa5294e24f0d626e4050
asteriusj <jstewart@asteriusmedia.com>
2016-11-27 17:58:47 -0600

    Update {id}.js

--- handlers/strategery/{id}.js
+++ handlers/strategery/{id}.js
@@ -3,7 +3,7 @@
  var repository = require('../../lib/strategeryRepository');
 
  module.exports = {
-     get: function getStrategery(req, res) {
+     get: function getStrategeryById(req, res) {
          res.json(repository.get(req.params['id']));
      }    
  };
