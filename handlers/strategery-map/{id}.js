ffda097aec047e47da7e739aef38a7156a9851ae
asteriusj <jstewart@asteriusmedia.com>
2016-11-28 17:03:12 -0600

    Delete {id}.js

--- handlers/strategery-map/{id}.js
+++
@@ -1,9 +0,0 @@
- 'use strict';
-
- var repository = require('../../lib/strategery#mapRepository');
-
- module.exports = {
-     get: function StrategeryMap_getById(req, res) {
-         res.json(repository.get(req.params['id']));
-     }    
- };
