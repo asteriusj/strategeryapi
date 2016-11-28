00c1d31e8e83f2775ab371236e4a18316a061321
asteriusj <jstewart@asteriusmedia.com>
2016-11-28 16:53:17 -0600

    Update {id}.js

--- handlers/strategery-map/{id}.js
+++ handlers/strategery-map/{id}.js
@@ -3,7 +3,7 @@
  var repository = require('../../lib/strategery#mapRepository');
 
  module.exports = {
-     get: function getStrategeryMapById(req, res) {
+     get: function StrategeryMap_getById(req, res) {
          res.json(repository.get(req.params['id']));
      }    
  };
